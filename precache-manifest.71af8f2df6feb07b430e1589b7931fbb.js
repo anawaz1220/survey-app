dfb8e4b1d1dde3622640ba4cacd45897b14e658a
Asif Nawaz <asifnawaz1220@gmail.com>
2023-10-11 12:34:28 +0500

    Initial commit

---
+++ precache-manifest.71af8f2df6feb07b430e1589b7931fbb.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf0f02bed492e08d420b8d41c1eb820",
+    "url": "DB1.png"
+  },
+  {
+    "revision": "58fa529026dc4204978c7d9099c6f75a",
+    "url": "DoubleArmStreetLightPole.png"
+  },
+  {
+    "revision": "310cd767b8911ff86d0c17b2dfdf9fc9",
+    "url": "FSW- Potential Host spots.png"
+  },
+  {
+    "revision": "8b0c583220498cc60fe6cfcc616ee924",
+    "url": "GENERATOR.png"
+  },
+  {
+    "revision": "aa9dd019c6b3b86e9be34352cfedde07",
+    "url": "HouseConnection.png"
+  },
+  {
+    "revision": "4c828fb7e815a02ba9214b3b6d99f1ab",
+    "url": "HouseConnection1.png"
+  },
+  {
+    "revision": "19e3762a390cb3a7dadd91b540c6b57f",
+    "url": "OrdinaryEarth.png"
+  },
+  {
+    "revision": "a59b40f02e2debdbf35dbfc67f1004b0",
+    "url": "People Who Inject Drugs.png"
+  },
+  {
+    "revision": "ddbca93246c22ad9372d41d909c11143",
+    "url": "Screenshot_7 copy.png"
+  },
+  {
+    "revision": "300057eb3b10dfb75d869c451ebeebbb",
+    "url": "SepticTank.png"
+  },
+  {
+    "revision": "39ba19b0c0b76895a6b0a50182cf7b50",
+    "url": "SpacialEarth.png"
+  },
+  {
+    "revision": "f168c12d17b3d51c56618eb5065c1e84",
+    "url": "Transformer.png"
+  },
+  {
+    "revision": "5e726939255c337738b81dcd38dcc54a",
+    "url": "Transgender.png"
+  },
+  {
+    "revision": "9f52c2cb57d5232a0888",
+    "url": "css/app.d64da77e.css"
+  },
+  {
+    "revision": "244dc64e068841f46104",
+    "url": "css/chunk-vendors.aa24aac7.css"
+  },
+  {
+    "revision": "c299315f2c6bd84a442cefb66c869969",
+    "url": "data/Plots_Approved.json"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "fdb87582932b354e03892f165c70d5de",
+    "url": "index.html"
+  },
+  {
+    "revision": "6e1737ca725ff8854a09",
+    "url": "js/about.4e8c057c.js"
+  },
+  {
+    "revision": "9f52c2cb57d5232a0888",
+    "url": "js/app.2207dc0f.js"
+  },
+  {
+    "revision": "244dc64e068841f46104",
+    "url": "js/chunk-vendors.1bdf7eb2.js"
+  },
+  {
+    "revision": "554fa87c776c032b160248efebff29a5",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "2d638dedca36144aa2072c8da2ecb327",
+    "url": "server.js"
+  },
+  {
+    "revision": "df7767e54f9e94c07d6d96518cfbcb10",
+    "url": "valve-1.png"
+  }
+]);
